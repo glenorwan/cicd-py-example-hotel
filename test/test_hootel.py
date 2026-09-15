5db7881e609edc4b1f3dd40bdd68e8219d83dad0
CsapoBence <csapobence98@gmail.com>
2026-09-15 17:40:49 +0200

    set window size

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -11,8 +11,11 @@ class TestHootel(object):
         URL = 'http://hotel-v3.progmasters.hu/'
         options = Options()
         options.add_experimental_option("detach", True)
+        options.add_argument("--guest")
+        options.add_argument("--headless")
         self.browser = webdriver.Chrome(options=options)
         self.browser.get(URL)
+        self.browser.set_window_size(1000, 1000)
 
     def teardown_method(self):
         self.browser.quit()
